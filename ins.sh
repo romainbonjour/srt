2820228c2a259a95c7f76e8aa27912b5cf9fdec6
rbr <romain.bonjour@outlook.com>
2020-08-19 16:50:04 +0200

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -1,4 +1,4 @@
 #First time instalation script
 echo "First time installation script"
-wget https://code-srt.scapp.swisscom.com/src/run.sh
+wget -s https://code-srt.scapp.swisscom.com/src/run.sh
 bash run.sh
