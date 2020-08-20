2f5b24c9af850c557b17de9f34208e86ab42b37e
rbr <romain.bonjour@outlook.com>
2020-08-20 11:12:01 +0200

    Update ins.sh

--- ins.sh
+++ ins.sh
@@ -2,5 +2,6 @@
 echo "First time installation script"
 curl https://code-srt.scapp.swisscom.com/src/run.sh -o run.sh -s
 echo "run.sh downloaded, running the script..."
-source alias run="bash run.sh"
+shopt -s expand_aliases
+alias run="bash run.sh"
 run
